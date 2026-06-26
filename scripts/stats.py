01aa424c012ea19b6bfa078816a6d5fcb34821ec
Jakob Hauser <jakob@hauser.earth>
2026-06-26 14:57:44 +0200

    stats

--- scripts/stats.py
+++ scripts/stats.py
@@ -2,12 +2,6 @@ from enum import Enum, auto
 from sqlalchemy import text
 
 
-class Query_Ret(Enum):
-    SCALAR = auto()
-    LIST = auto()
-    DICT = auto()
-
-
 def to_scalar(rows):
     return rows[0][0] if rows else None
 
@@ -20,18 +14,11 @@ def to_dict(rows):
     return {r[0]: r[1] for r in rows} if rows else None
 
 
-HANDLERS = {
-    Query_Ret.SCALAR: to_scalar,
-    Query_Ret.LIST: to_list,
-    Query_Ret.DICT: to_dict,
-}
-
-
-def collect_stats(stats_engine, stats_queries: Dict[str, str]):
+def collect_stats(stats_engine, stats_queries: dict[str, str]):
     res = dict()
     with engine.connect() as conn:
-        for name, (q_type, sql) in stats_queries.items():
-            res[name] = HANDLERS[q_type](conn.execute(text(sql)).fetchall())
+        for name, (q_parser, sql) in stats_queries.items():
+            res[name] = q_parser(conn.execute(text(sql)).fetchall())
     return res
 
 
@@ -41,26 +28,26 @@ if __name__ == "__main__":
 
     stats_queries = {
         "n_sub": (
-            Query_Ret.SCALAR,
+            to_scalar,
             """
                   SELECT COUNT(*) from substance
                   """,
         ),
         "n_geloc": (
-            Query_Ret.SCALAR,
+            to_scalar,
             """
                    SELECT COUNT(address) FROM public.company
                    """,
         ),
         "n_sub_struct": (
-            Query_Ret.SCALAR,
+            to_scalar,
             """
                          SELECT COUNT(*) FROM public.substance
                          WHERE canonical_smiles IS NOT NULL OR struct_type = 'SEQ'
                          """,
         ),
         "n_sub_DBid": (
-            Query_Ret.SCALAR,
+            to_scalar,
             """
                        SELECT COUNT(*) FROM public.substance s
                        WHERE COALESCE(pubchem_cid, pubchem_sid, chembl_id) IS NOT NULL
